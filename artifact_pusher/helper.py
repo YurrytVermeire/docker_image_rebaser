eb85e46c9706f04957ccb91dc6c57f9299467466
YurrytVermeire <yurryt.vermeire@gmail.com>
2022-05-02 16:02:54 +0200

    Added Documentation

--- artifact_pusher/helper.py
+++ artifact_pusher/helper.py
@@ -1,5 +1,11 @@
 
 def parse_image_input(image_string):
+    """
+    It takes a string as input and returns a list with the image name and tag split
+    
+    :param image_string: The string that contains the image name and tag
+    :return: A list with the image name and tag split.
+    """
     parsed_string = list()
     if ":" in image_string:
         parsed_string = image_string.split(":")
--- artifact_pusher/images.py
+++ artifact_pusher/images.py
@@ -4,6 +4,12 @@ import time
 
 
 def initiate_docker_client(base_url):
+    """
+    It creates a docker client object that can be used to interact with the docker daemon
+    
+    :param base_url: The URL of the Docker daemon
+    :return: A docker client object.
+    """
     print(42*"-")
     print("Connecting to the docker client.")
     try:
@@ -12,16 +18,31 @@ def initiate_docker_client(base_url):
         print("Connected!")
 
 
-def pull_image(local_images, name, tag="latest"):
+def pull_image(docker, name, tag="latest"):
+    """
+    > Pulls a docker image from the docker hub
+    
+    :param docker: The docker client object
+    :param name: The name of the image to pull
+    :param tag: The tag of the image to pull, defaults to latest (optional)
+    :return: The image ID of the pulled image.
+    """
     print(42*"-")
     print("Pulling image, this may take a few minutes.")
     try:
-        return local_images.pull(name, tag=tag)
+        return docker.pull(name, tag=tag)
     finally:
         print("Done pulling image!")
 
 
 def retag_image(docker, image, repo):
+    """
+    It takes a docker image, and retags it with a new repository name
+    
+    :param docker: docker client
+    :param image: This is the image that you want to retag. It's a tuple of the image name and the tag
+    :param repo: The repository to push the image to
+    """
     print(42*"-")
     print("Retagging image...")
     time.sleep(5)
@@ -32,6 +53,13 @@ def retag_image(docker, image, repo):
 
 
 def push_image(docker, image, repo):
+    """
+    This function takes a docker image and a repository as input, and pushes the image to the repository
+    
+    :param docker: The docker client object
+    :param image: The name of the image you want to push
+    :param repo: The name of the repository you want to push to
+    """
     print(42*"-")
     print("Pushing image, this may take a few minutes.")
     docker.push(repo, tag=image)
